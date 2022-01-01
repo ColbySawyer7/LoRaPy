7b1df518e1dd25f13a2b8a6189e7d6548806b5b1
Adam Novak <interfect@gmail.com>
2021-12-31 22:21:09 -0500

    Require spi-lora that actually has reset

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
           'spidev',
           'argparse',
           'LoRaWAN',
-          'spi-lora>=0.2.0',
+          'spi-lora>=0.2.1',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
